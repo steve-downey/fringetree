38b181c1fb5689cd8b9a2bd3f0467399d23d1325
Steve Downey <sdowney@gmail.com>
2018-07-24 16:15:56 -0400

    Factory functions for shared_ptr

--- fringetree/fringetree.h
+++ fringetree/fringetree.h
@@ -22,7 +22,12 @@ class Branch {
     std::shared_ptr<Tree<Tag, Value>> left_;
     std::shared_ptr<Tree<Tag, Value>> right_;
   public:
-    auto tag() const -> Tag {return tag_;}
+    Branch() : tag_(0), left_(0), right_(0) {}
+    Branch(Tag                               tag,
+           std::shared_ptr<Tree<Tag, Value>> left,
+           std::shared_ptr<Tree<Tag, Value>> right)
+        : tag_(tag), left_(left), right_(right) {}
+    auto tag() const -> Tag { return tag_; }
 };
 
 template<typename Tag, typename Value>
@@ -30,6 +35,8 @@ class Leaf {
     Tag tag_;
     Value v_;
   public:
+    Leaf() : tag_(0), v_(0) {};
+    Leaf(Tag tag, Value v) : tag_(tag), v_(v) {}
     auto tag() const -> Tag {return tag_;}
 };
 
@@ -51,8 +58,19 @@ class Tree {
             },
             data);
     }
+
+    static auto leaf(Value const& v) -> std::shared_ptr<Tree> {
+        return std::make_shared<Tree>(Leaf_(1, v));
+    }
+
+    static auto branch(std::shared_ptr<Tree> left, std::shared_ptr<Tree> right)
+        -> std::shared_ptr<Tree> {
+        return std::make_shared<Tree>(
+            Branch_((left->tag() + right->tag()), left, right));
+    }
 };
 
+constexpr auto tag = [](auto tree) {return tree->tag();};
 
 // ============================================================================
 //              INLINE FUNCTION AND FUNCTION TEMPLATE DEFINITIONS
--- fringetree/fringetree.t.cpp
+++ fringetree/fringetree.t.cpp
@@ -15,5 +15,28 @@ TEST(TreeTest, Breathing) {
     Tree<int, int> t2{branch};
 
     int ltag = t1.tag();
-    int btag = t1.tag();
+    int btag = t2.tag();
+
+    ASSERT_EQ(0, ltag);
+    ASSERT_EQ(0, btag);
+
+}
+
+TEST(TreeTest, makeLeaf) {
+    using Tree = Tree<int, int>;
+    std::shared_ptr<Tree> p;
+    auto p2 = Tree::leaf(1);
+
+    ASSERT_EQ(1, p2->tag());
+    ASSERT_EQ(tag(p2), p2->tag());
+}
+
+TEST(TreeTest, makeBranch) {
+    using Tree = Tree<int, int>;
+    auto l1 = Tree::leaf(1);
+    auto l2 = Tree::leaf(2);
+    auto b1 = Tree::branch(l1, l2);
+    ASSERT_EQ(2, b1->tag());
+    ASSERT_EQ(tag(b1), b1->tag());
+
 }
