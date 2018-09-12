d1edb21ece5b383a38522f4c3f5f85e06e8b6d77
Steve Downey <sdowney@gmail.com>
2018-09-12 18:28:21 -0400

    Split the examples
    Multiple example excutuables

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -23,22 +23,4 @@ int main(int /*argc*/, char** /*argv*/)
     t1->visit(p);
     t2->visit(p);
     std::cout << "}\n";
-
-
-    auto list = Tree::empty();
-    auto l1 = prepend(0, list);
-    auto l2 = prepend(1, l1);
-    auto l3 = prepend(2, l2);
-    auto l4 = prepend(3, l3);
-    auto l5 = prepend(4, l4);
-
-    std::cout << "digraph G {\n";
-    printer_ p2(std::cout);
-    list->visit(p2);
-    l1->visit(p2);
-    l2->visit(p2);
-    l3->visit(p2);
-    l4->visit(p2);
-    l5->visit(p2);
-    std::cout << "}\n";
 }
